71939ccdc8515c1348ba6b87e8680553b226a0d4
Seongyeop Jeong <jeon0042@algonquinlive.com>
2019-05-02 23:54:02 -0400

    adjust material ui, merged classes into Customer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    witdth: '100%',
+    marginTop: theme.spacing.unit *3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+    // 가로 길이가 1080 보다 작아지면 스크롤이 나오게 하는 css
+  }
+})
 
 const customers = [
   {
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
   'name': 'James',
-  'brithday': '871222',
+  'birthday': '871222',
   'gender': 'male',
   'job': 'student'
   },
@@ -16,7 +35,7 @@ const customers = [
     'id': 2,
     'image': 'https://placeimg.com/64/64/2',
     'name': 'John',
-    'brithday': '911212',
+    'birthday': '911212',
     'gender': 'male',
     'job': 'student'
   },
@@ -24,7 +43,7 @@ const customers = [
     'id': 3,
     'image': 'https://placeimg.com/64/64/3',
     'name': 'Ahnaf',
-    'brithday': '921212',
+    'birthday': '921212',
     'gender': 'male',
     'job': 'student'
   }
@@ -32,26 +51,29 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-              );
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Image</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Birthday</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Job</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        { customers.map(c => {
+            return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+            }) }
+        </TableBody>
+      </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
