38b3026b4be343595d48e71f1522c882b156f448
seongyeopjeong <jamesjeong713@gmail.com>
2019-05-04 19:08:12 -0400

    add RESTful api with GET method for customers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer'
 import Table from '@material-ui/core/Table';
@@ -9,6 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 import { Paper } from '@material-ui/core';
+// import { async } from 'rxjs/internal/scheduler/async';
 
 const styles = theme => ({
   root: {
@@ -20,38 +21,30 @@ const styles = theme => ({
     minWidth: 1080
     // 가로 길이가 1080 보다 작아지면 스크롤이 나오게 하는 css
   }
-})
-
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'James',
-  'birthday': '871222',
-  'gender': 'male',
-  'job': 'student'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': 'John',
-    'birthday': '911212',
-    'gender': 'male',
-    'job': 'student'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': 'Ahnaf',
-    'birthday': '921212',
-    'gender': 'male',
-    'job': 'student'
-  }
-]
+});
 
 class App extends Component {
+
+  // state 는 변경될 수 있는 데이터를 명시할 때,
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
-    const { classes } = this.props;
+    // props는 변경될 수 없는 데이터를 명시할 때,
+    const { classes } = this.props; 
     return (
       <Paper className={classes.root}>
       <Table className={classes.table}>
@@ -66,9 +59,9 @@ class App extends Component {
           </TableRow>
         </TableHead>
         <TableBody>
-        { customers.map(c => {
-            return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
-            }) }
+        { this.state.customers ? this.state.customers.map(c => { 
+          return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+        }) : "N/A" }
         </TableBody>
       </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res)=> {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res)=> {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': 'James',
+        'birthday': '871222',
+        'gender': 'male',
+        'job': 'student'
+        },
+        {
+          'id': 2,
+          'image': 'https://placeimg.com/64/64/2',
+          'name': 'John',
+          'birthday': '911212',
+          'gender': 'male',
+          'job': 'student'
+        },
+        {
+          'id': 3,
+          'image': 'https://placeimg.com/64/64/3',
+          'name': 'Ahnaf',
+          'birthday': '921212',
+          'gender': 'male',
+          'job': 'student'
+        }
+    ]);
 });
 // listening port
 app.listen (port, () => console.log(`Listening on port ${port}`));
