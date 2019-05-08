d38f50e094915f148a19221fbddd8c5918ea514a
seongyeopjeong <jamesjeong713@gmail.com>
2019-05-08 10:50:50 -0400

    Updates

---
+++ precache-manifest.9eef1f2783915050f0a69d2c7172e62c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dac3a612d48a0bd40970ac9ca06d150",
+    "url": "/react-management/index.html"
+  },
+  {
+    "revision": "7944834b972b2ea6c318",
+    "url": "/react-management/static/css/main.572d2d58.chunk.css"
+  },
+  {
+    "revision": "fe3625517554e3e63b55",
+    "url": "/react-management/static/js/2.ef3ee1dd.chunk.js"
+  },
+  {
+    "revision": "7944834b972b2ea6c318",
+    "url": "/react-management/static/js/main.17fb0547.chunk.js"
+  },
+  {
+    "revision": "5aaf95c8fdab4367aaca",
+    "url": "/react-management/static/js/runtime~main.d5f39466.js"
+  }
+]);
